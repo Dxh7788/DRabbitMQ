3d02b7be4b541cd6e78315d9e6e37dab7709b827
Dxh7788 <anan1215>
2018-01-08 15:57:30 +0800

    all rabbitmq topics

---
+++ src/main/java/com/drabbitmq/helloworld/Send.java
@@ -0,0 +1,27 @@
+package com.drabbitmq.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author xh.d
+ * @date 2018/1/8 15:36
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+        channel.close();
+        connection.close();
+    }
+}
